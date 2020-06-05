d59600ee9192739897500c336cb797fbdbea259f
Senadin <beganovicsenadin24@gmail.com>
2020-06-05 15:17:11 +0200

    added another page, added barba.js for transitions between pages, fixed css issues

--- app.js
+++ app.js
@@ -107,23 +107,75 @@ function activeCursor(e) {
   }
   if (item.classList.contains("explore")) {
     mouse.classList.add("explore-active");
-    gsap.to('.title-swipe', 1, {y: '0%'})
+    gsap.to(".title-swipe", 1, { y: "0%" });
     mouseTxt.innerText = "Tap";
   } else {
     mouse.classList.remove("explore-active");
     mouseTxt.innerText = "";
-    gsap.to('.title-swipe', 1, {y: '100%'})
+    gsap.to(".title-swipe", 1, { y: "100%" });
   }
 }
-function navToggle(e){
-  gsap.to('.line1', 0.5, {rotate: "45", y:5 , background: "balck"})
-  gsap.to('.line2', 0.5, {rotate: "-45", y:-5 , background: "black"})
-  gsap.to('.nav-bar', 1, {clipPath: 'circle(2500px at 100% -10%)'})
+function navToggle(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    document.body.classList.add("hide");
+  } else {
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to("#logo", 1, { color: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+    document.body.classList.remove("hide");
+  }
 }
 
-burger.addEventListener("click", navToggle)
+//Barba Page Transitions
+const logo = document.querySelector('#logo');
+barba.init({
+  views: [
+    {
+      namespace: 'home',
+      beforeEnter(){
+        animateSlides();
+        logo.href = './index.html';
+      },
+      beforeLeave(){
+        slideScene.destory();
+        pageScene.destory();
+        controller.destory();
+      },
+    },
+    {
+      namespace: 'fashion',
+      beforeEnter(){
+        logo.href = "../index.html"
+      }
+    }
+  ],
+  transitions: [
+    {
+      leave({current, next}){
+        let done = this.async();
+        // Scroll to the top
+        window.scrollTo(0, 0);
+        // An animation
+        const tl = gsap.timeline({defaults: { ease: "power2.inOut"}});
+        tl.fromTo(current.container, 1,  { opacity: 1 }, { opacity: 0, onComplete: done });
+      },
+      enter({current, next}){
+        let done = this.async();
+        //An animation
+        const tl = gsap.timeline({defaults: { ease: "power2.inOut"}});
+        tl.fromTo(next.container, 1,  { opacity: 0 }, { opacity: 1,onComplete: done});
+      }
+    }
+  ]
+})
+
+burger.addEventListener("click", navToggle);
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mouseover", activeCursor);
-
-
-animateSlides();
